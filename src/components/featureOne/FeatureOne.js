80ec4b21e4ca29814704ba392242bf9db6560342
velmurgan pillai <karthikpillai1323@gmail.com>
2021-09-07 17:17:01 +0530

    Adds feature one component

---
+++ src/components/featureOne/FeatureOne.js
@@ -0,0 +1,132 @@
+import React from "react";
+import "./FeatureOne.css";
+
+function FeatureOne() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="text-center">
+          <h2 className="heading">Welcome to</h2>
+          <h3>Stage Coach Lodge</h3>
+          <br />
+          <br />
+        </div>
+        <div className="col-10 container-fluid">
+          <div className="body-content">
+            <h2>Stage Coach Lodge in Monterey</h2>
+            <h5>
+              The Stage Coach Lodge is a boutique lodge located right off of
+              Highway 1 in Downtown Monterey.
+            </h5>
+            <br />
+            <p>
+              It is our pleasure to welcome you and your family to the beautiful
+              Monterey Peninsula. The Stage Coach Lodge offers easy access to
+              the various tourist attractions located in and around the
+              Peninsula with newly remodeled air-conditioned hotel rooms. Simply
+              make a right turn onto Tenth Street when exiting the Downtown
+              Monterey exit off of Highway 1 and you’ll find our hotel nestled
+              in a quiet residential neighborhood within a short stroll to Lake
+              El Estero, Dennis the Menace Park, a variety of shops and
+              restaurants, and of course, the beach. Our Monterey hotel is also
+              within walking distance to the Naval Postgraduate School and Hyatt
+              Regency Conference Center. You can drive to downtown, Fisherman’s
+              Wharf, Cannery Row and the Monterey Bay Aquarium or hop back onto
+              HWY 1 to get to Laguna Seca Raceway, the Monterey Fairgrounds,
+              Carmel and the 17 Mile Drive, which are all within minutes of our
+              property.
+            </p>
+          </div>
+          <div className="text-right">
+            <h2>Stage Coach Lodge in Monterey</h2>
+            <h5>
+              The Stage Coach Lodge is a boutique lodge located right off of
+              Highway 1 in Downtown Monterey.
+            </h5>
+            <br />
+            <p>
+              It is our pleasure to welcome you and your family to the beautiful
+              Monterey Peninsula. The Stage Coach Lodge offers easy access to
+              the various tourist attractions located in and around the
+              Peninsula with newly remodeled air-conditioned hotel rooms. Simply
+              make a right turn onto Tenth Street when exiting the Downtown
+              Monterey exit off of Highway 1 and you’ll find our hotel nestled
+              in a quiet residential neighborhood within a short stroll to Lake
+              El Estero, Dennis the Menace Park, a variety of shops and
+              restaurants, and of course, the beach. Our Monterey hotel is also
+              within walking distance to the Naval Postgraduate School and Hyatt
+              Regency Conference Center. You can drive to downtown, Fisherman’s
+              Wharf, Cannery Row and the Monterey Bay Aquarium or hop back onto
+              HWY 1 to get to Laguna Seca Raceway, the Monterey Fairgrounds,
+              Carmel and the 17 Mile Drive, which are all within minutes of our
+              property.
+            </p>
+          </div>
+          <div className="text-right">
+            <h2>Monterey Hotel Rooms</h2>
+            <br />
+            <p>
+              This Lodge offers a plentiful selection of well-appointed and
+              newly remodeled room types from a King Honeymoon Suite to Standard
+              Double Rooms that offer accommodations for up to four guests. All
+              rooms are equipped with air-conditioning, microwave/refrigerators,
+              42 inch 1080i HD LCD cable TV, Keurig coffee maker, high-speed
+              wireless internet, free local calls, in-room safe, and iron &
+              ironing boards and granite bathrooms with full baths with hair
+              dryers. The Stage Coach Lodge also offers an outdoor swimming pool
+              and great landscaping and on-site laundry. The Lodge also has a
+              diver’s wash station for those who are diving in The Bay and a
+              small Patio for those who just want to relax by our pool.
+            </p>
+            <br />
+            <p>
+              Spacious and comfortable rooms and suites make the Stage Coach
+              Lodge the perfect setting for family reunions, corporate events,
+              or a romantic gateway. Our management and staff take great
+              satisfaction in providing our guests with the highest levels of
+              professional service and hospitality. We have also just recently
+              added a Diver's Quarters with a full dive equipment washbasin and
+              showers so you can make the Stage Coach Lodge your place to stay
+              when diving off the Monterey Coast. The staff and management of
+              the Stage Coach Lodge invite you to join us on your “coastal
+              experience”.
+            </p>
+          </div>
+          <div className="text-right">
+            <h2>Monterey Sightseeing & Tourism</h2>
+            <br />
+            <p>
+              The Monterey Peninsula offers the vacation experience of a
+              lifetime with a beauty that entices visitors from all over the
+              world. Monterey is located along the central coast of California
+              about 120 miles south of San Francisco. The Peninsula is one of
+              the most scenic areas of the world offering great ocean vistas and
+              the famed 17 Mile Drive, a coastal toll road that offers
+              breathtaking views of the bountiful Pacific Ocean and the rolling
+              green hills of the world-famous Pebble Beach and Spyglass Golf
+              Courses.
+            </p>
+          </div>
+          <div className="text-right">
+            <h2>Monterey, CA Attractions</h2>
+            <br />
+            <p>
+              Monterey is also home to the world's largest sea life exhibit, the
+              Monterey Bay Aquarium. Pacific Grove, the host to one of the
+              oldest working lighthouses on the west coast, comes alive when the
+              monarch butterfly populations migrate to this tiny coastal town.
+              Carmel offers a picturesque white sand beach and old-world charm
+              with its quaint shops. These spectacular beaches, nine world
+              champion golf courses, historic old adobes, Fisherman's Wharf,
+              infamous Cannery Row, fabulous shopping, and gourmet restaurants
+              all await you. And The Stage Coach Lodge offers the perfect quaint
+              and memorable place to stay when visiting Monterey.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FeatureOne;
